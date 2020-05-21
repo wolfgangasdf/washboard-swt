3c3e5f89c65567e996617f81dd6bf90fceeaf1a2
wolfgang <wolfgang.loeffler@gmail.com>
2020-05-21 10:19:50 +0200

    updates

---
+++ README.md
@@ -0,0 +1,27 @@
+```aidl
+
+
+palantir-graal doesnt work yet with graal 20
+do adapted hack: https://github.com/palantir/gradle-graal/issues/239#issuecomment-578493879		
+    export JAVA_HOME=/Library/Java/JavaVirtualMachines/graalvm-ce-java11-20.1.0/Contents/Home
+    export PATH="$JAVA_HOME/bin:${PATH}"
+    gu install native-image
+    mkdir -p ~/.gradle/caches/com.palantir.graal/20.1.0/graalvm-ce-20.1.0/bin
+    touch ~/.gradle/caches/com.palantir.graal/20.1.0/graalvm-ce-20.1.0-amd64.tar.gz
+    ln -s /Library/Java/JavaVirtualMachines/graalvm-ce-java11-20.1.0/Contents ~/.gradle/caches/com.palantir.graal/20.1.0/graalvm-ce-20.1.0/Contents
+    gradle clean nativeImage
+    # resulting bin in build/graal
+
+    # -> Warning: System method java.lang.System.loadLibrary invoked at org.eclipse.swt.internal.Library.load(Library.java:232)
+    #need "--no-fallback" in options...
+    gradle clean nativeImage
+        Exception in thread "main" java.lang.NoSuchFieldError: org.eclipse.swt.internal.cocoa.NSOperatingSystemVersion.patchVersion
+            at com.oracle.svm.jni.functions.JNIFunctions$Support.getFieldID(JNIFunctions.java:1107)
+=> graalvm & swt: need JNI config files, see 
+    https://github.com/oracle/graal/issues/2232
+    https://github.com/mbarbeaux/graalvm-swt-native-image
+    => wait until bug on mac resolved!
+    see graalvm-swt-native-image-master which is modified for graal-java11
+
+
+```
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-val kotlinversion = "1.3.70"
+val kotlinversion = "1.3.72"
 group = "com.wolle.washboard-swt"
 version = "1.0-SNAPSHOT"
 val cPlatforms = listOf("mac") // compile for these platforms. "mac", "linux", "win"
@@ -9,11 +9,11 @@ println("Current Java version: ${JavaVersion.current()}")
 if (JavaVersion.current().majorVersion.toInt() < 11) throw GradleException("Use Java >= 11")
 
 plugins {
-    kotlin("jvm") version "1.3.70"
+    kotlin("jvm") version "1.3.72"
     application
     id("com.github.ben-manes.versions") version "0.28.0"
-//    id("org.beryx.runtime") version "1.8.0"
-    id("com.palantir.graal") version "0.6.0-114-gfe95739"
+//    id("org.beryx.runtime") version "1.8.4"
+    id("com.palantir.graal") version "0.7.0"
 }
 
 repositories {
@@ -30,138 +30,22 @@ dependencies {
 //    implementation("de.brudaswen.kotlinx.coroutines:kotlinx-coroutines-swt:1.0.0")
     implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
     implementation("org.jetbrains.kotlin:kotlin-stdlib:$kotlinversion")
-    implementation("io.github.microutils:kotlin-logging:1.7.8")
+    implementation("io.github.microutils:kotlin-logging:1.7.9")
     implementation("org.slf4j:slf4j-simple:1.8.0-beta4") // no colors, everything stderr
 
-    implementation("org.eclipse.platform:org.eclipse.swt.cocoa.macosx.x86_64:3.113.0") {
+    implementation("org.eclipse.platform:org.eclipse.swt.cocoa.macosx.x86_64:3.114.0") {
         isTransitive = false
     }
 
 }
 
 graal {
-    graalVersion("20.0.0")
-    downloadBaseUrl("https://github.com/graalvm/graalvm-ce-builds/releases/download/")
-    // https://github.com/graalvm/graalvm-ce-builds/releases/download/vm-20.0.0/graalvm-ce-java11-darwin-amd64-20.0.0.tar.gz
-    // https://github.com/graalvm/graalvm-ce-builds/releases/download//vm-20.0.0/graalvm-ce-darwin-amd64-20.0.0.tar.gz
-    option("--no-fallback")
+    graalVersion("20.1.0")
+//    option("--no-fallback")
+    option("-J-XstartOnFirstThread")
     mainClass("WashboardSwtMainKt")
     outputName("washboard-swt")
 }
-
-/*
-runtime {
-    options.set(listOf("--strip-debug", "--compress", "2", "--no-header-files", "--no-man-pages"))
-    // first row: suggestModules
-    modules.set(listOf("java.desktop")) //"java.logging", "java.prefs", "java.xml", "jdk.unsupported", "jdk.jfr", "jdk.jsobject", "jdk.xml.dom",
-//            "jdk.crypto.cryptoki","jdk.crypto.ec")) // needed?
-
-    if (cPlatforms.contains("mac")) targetPlatform("mac", System.getenv("JDK_MAC_HOME"))
-    if (cPlatforms.contains("win")) targetPlatform("win", System.getenv("JDK_WIN_HOME"))
-    if (cPlatforms.contains("linux")) targetPlatform("linux", System.getenv("JDK_LINUX_HOME"))
-}
-*/
-
-//open class CrossPackage : DefaultTask() {
-//    @org.gradle.api.tasks.Input var execfilename = "execfilename"
-//    @org.gradle.api.tasks.Input var macicnspath = "macicnspath"
-//
-//    @TaskAction
-//    fun crossPackage() {
-//        File("${project.buildDir.path}/crosspackage/").mkdirs()
-//        project.runtime.targetPlatforms.get().forEach { (t, _) ->
-//            println("targetplatform: $t")
-//            val imgdir = "${project.runtime.imageDir.get()}/${project.name}-$t"
-//            println("imagedir: $imgdir")
-//            when(t) {
-//                "mac" -> {
-//                    val appp = File(project.buildDir.path + "/crosspackage/mac/$execfilename.app").path
-//                    project.delete(appp)
-//                    project.copy {
-//                        into(appp)
-//                        from(macicnspath) {
-//                            into("Contents/Resources").rename { "$execfilename.icns" }
-//                        }
-//                        from("$imgdir/${project.application.executableDir}/${project.application.applicationName}") {
-//                            into("Contents/MacOS")
-//                        }
-//                        from(imgdir) {
-//                            into("Contents")
-//                        }
-//                    }
-//                    val pf = File("$appp/Contents/Info.plist")
-//                    pf.writeText("""
-//                        <?xml version="1.0" ?>
-//                        <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-//                        <plist version="1.0">
-//                         <dict>
-//                          <key>LSMinimumSystemVersion</key>
-//                          <string>10.9</string>
-//                          <key>CFBundleDevelopmentRegion</key>
-//                          <string>English</string>
-//                          <key>CFBundleAllowMixedLocalizations</key>
-//                          <true/>
-//                          <key>CFBundleExecutable</key>
-//                          <string>$execfilename</string>
-//                          <key>CFBundleIconFile</key>
-//                          <string>$execfilename.icns</string>
-//                          <key>CFBundleIdentifier</key>
-//                          <string>${project.group}</string>
-//                          <key>CFBundleInfoDictionaryVersion</key>
-//                          <string>6.0</string>
-//                          <key>CFBundleName</key>
-//                          <string>${project.name}</string>
-//                          <key>CFBundlePackageType</key>
-//                          <string>APPL</string>
-//                          <key>CFBundleShortVersionString</key>
-//                          <string>${project.version}</string>
-//                          <key>CFBundleSignature</key>
-//                          <string>????</string>
-//                          <!-- See http://developer.apple.com/library/mac/#releasenotes/General/SubmittingToMacAppStore/_index.html
-//                               for list of AppStore categories -->
-//                          <key>LSApplicationCategoryType</key>
-//                          <string>Unknown</string>
-//                          <key>CFBundleVersion</key>
-//                          <string>100</string>
-//                          <key>NSHumanReadableCopyright</key>
-//                          <string>Copyright (C) 2019</string>
-//                          <key>NSHighResolutionCapable</key>
-//                          <string>true</string>
-//                          <key>NSAppTransportSecurity</key>
-//                          <dict>
-//                            <key>NSAllowsArbitraryLoads</key>
-//                            <true/>
-//                          </dict>
-//                         </dict>
-//                        </plist>
-//                    """.trimIndent())
-//                    // touch folder to update Finder
-//                    File(appp).setLastModified(System.currentTimeMillis())
-//                    // zip it
-//                    org.gradle.kotlin.dsl.support.zipTo(File("${project.buildDir.path}/crosspackage/$execfilename-mac.zip"), File("${project.buildDir.path}/crosspackage/mac"))
-//                }
-//                "win" -> {
-//                    org.gradle.kotlin.dsl.support.zipTo(File("${project.buildDir.path}/crosspackage/$execfilename-win.zip"), File(imgdir))
-//                }
-//                "linux" -> {
-//                    org.gradle.kotlin.dsl.support.zipTo(File("${project.buildDir.path}/crosspackage/$execfilename-linux.zip"), File(imgdir))
-//                }
-//            }
-//        }
-//    }
-//}
-
-//tasks.register<CrossPackage>("crosspackage") {
-//    dependsOn("runtime")
-//    execfilename = "washboard-swt"
-//    macicnspath = "./icon.icns"
-//}
-
-//tasks.withType(CreateStartScripts::class).forEach {script ->
-//    script.doFirst {
-//        script.classpath =  files("lib/*")
-//    }
-//}
 
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
